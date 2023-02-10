7056c7f2ece7f4682ce0698ffd81e0ea779ce1ad
niels <leonardolarsen9605@outlook.com>
2023-02-10 16:30:21 -0600

    created functional loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io::stdin;
 fn main() {
-    println!("Hello, world!");
+    loop {
+        println!("Do  you wish to continue running the program? (y/n:): ");
+        let mut user_input = String::new();
+        stdin()
+            .read_line(&mut user_input)
+            .expect("Failed to read line");
+
+        if user_input.trim().to_lowercase() == "y" {
+            //place more shitty code here
+            println!("The loop is functional");
+        } else if user_input.trim().to_lowercase() == "n" {
+            break;
+        }
+    }
 }
